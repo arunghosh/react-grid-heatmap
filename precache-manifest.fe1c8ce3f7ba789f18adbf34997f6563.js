5de0d9c1b0bb47161df38fd6628521ca594788f0
Arun Ghosh <arunghosh@gmail.com>
2020-07-02 11:53:45 +0530

    Updates

---
+++ precache-manifest.fe1c8ce3f7ba789f18adbf34997f6563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2224c0119a784a380bdeabc030a3f0db",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b0df535fd64361c2463",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "9c8cb7e8675de3d58479",
+    "url": "./static/js/2.ca4410cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ca4410cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b0df535fd64361c2463",
+    "url": "./static/js/main.3ef43778.chunk.js"
+  },
+  {
+    "revision": "f0dabbde67ea2fdb712e",
+    "url": "./static/js/runtime-main.561ca738.js"
+  }
+]);
